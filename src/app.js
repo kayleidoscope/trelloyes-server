9907f8150e5f7dfb8ecbe341dc01881be6c846ea
Kay Holten <holtenkay@gmail.com>
2020-11-19 17:01:52 -0800

    get and post endpoints created

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const { restart } = require('nodemon')
 const winston = require('winston');
+const { v4: uuid } = require('uuid');
 
 const app = express()
 
@@ -32,6 +33,7 @@ if (NODE_ENV !== 'production') {
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json());
 
 const cards = [{
     id: 1,
@@ -61,6 +63,71 @@ app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
+/* ------------ /card ENDPOINTS ------------ */
+
+app.get('/card', (req, res) => {
+    res
+      .json(cards);
+});
+
+app.get('/card/:id', (req, res) => {
+    const { id } = req.params;
+    const card = cards.find(c => c.id == id);
+  
+    // make sure we found a card
+    if (!card) {
+      logger.error(`Card with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Card Not Found');
+    }
+  
+    res.json(card);
+});
+
+app.post('/card', (req, res) => {
+    const { title, content } = req.body;
+
+    if (!title) {
+        logger.error(`Title is required`);
+        return res
+          .status(400)
+          .send('Invalid data');
+    }
+      
+    if (!content) {
+        logger.error(`Content is required`);
+        return res
+          .status(400)
+          .send('Invalid data');
+    }
+
+    // get an id
+    const id = uuid();
+
+    const card = {
+        id,
+        title,
+        content
+    };
+
+    cards.push(card);
+
+    logger.info(`Card with id ${id} created`);
+
+    res
+        .status(201)
+        .location(`http://localhost:8000/card/${id}`)
+        .json(card);
+})
+  
+/* ------------ /list ENDPOINTS ------------ */
+
+app.get('/list', (req, res) => {
+    res
+      .json(lists);
+});
+
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
@@ -71,5 +138,67 @@ app.use(function errorHandler(error, req, res, next) {
     }
     res.status(500).json(response)
 })
+
+app.get('/list/:id', (req, res) => {
+    const { id } = req.params;
+    const list = lists.find(li => li.id == id);
+  
+    // make sure we found a list
+    if (!list) {
+      logger.error(`List with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('List Not Found');
+    }
+  
+    res.json(list);
+});
+
+app.post('/list', (req, res) => {
+    const { header, cardIds = [] } = req.body;
+  
+    if (!header) {
+      logger.error(`Header is required`);
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+  
+    // check card IDs
+    if (cardIds.length > 0) {
+      let valid = true;
+      cardIds.forEach(cid => {
+        const card = cards.find(c => c.id == cid);
+        if (!card) {
+          logger.error(`Card with id ${cid} not found in cards array.`);
+          valid = false;
+        }
+      });
+  
+      if (!valid) {
+        return res
+          .status(400)
+          .send('Invalid data');
+      }
+    }
+  
+    // get an id
+    const id = uuid();
+  
+    const list = {
+      id,
+      header,
+      cardIds
+    };
+  
+    lists.push(list);
+  
+    logger.info(`List with id ${id} created`);
+  
+    res
+      .status(201)
+      .location(`http://localhost:8000/list/${id}`)
+      .json({id});
+});
 
 module.exports = app
